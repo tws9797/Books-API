51bb18d3269284eae46ea9c61cc60a6ec848b0a1
tws9797 <twenseng7227@gmail.com>
2019-04-25 15:14:24 +0800

    Migration

---
+++ database/migrations/2019_04_25_070701_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('isbn')->unique();
+            $table->string('title')->indexed();
+            $table->integer('year')->indexed();
+            $table->unsignedInteger('publisher_id');
+            $table->foreign('publisher_id')->references('id')->on('publishers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
