9ba1eaa423a387f35b417df8f144d3148e235e6c
tws9797 <twenseng7227@gmail.com>
2019-04-26 16:00:53 +0800

    Authentication JWT

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('books', 'BookController');
-Route::apiResource('publishers', 'PublisherController');
-Route::apiResource('authors', 'AuthorController');
+Route::middleware('api')->namespace('Auth')->prefix('auth')->group(function(){
+  Route::post('me', 'AuthController@me');
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+});
+
+Route::middleware('jwt.auth')->group(function(){
+  Route::apiResource('books', 'BookController');
+  Route::apiResource('publishers', 'PublisherController');
+  Route::apiResource('authors', 'AuthorController');
+});
